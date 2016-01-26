c133fab25c2ff499eddb9cca20a33a3f2161ee4f
Holly-jolly <holly.haixin.l@gmail.com>
2016-01-25 23:38:09 -0800

    add a form to add a new guest

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -9,8 +9,49 @@
   <script src="public/javascript/bootstrap.min.js"></script>
 </head>
 <body>
+  <h1> Guestbook </h1>
+    <div>Company Name: H for Hope Independent Consultant </div>
+      <div> Add a new guest: </div>
+      <form class="form-horizontal" method="post" action="?">
+        <div class="control-group">
+          <label class="control-label">First name: </label>
+          <div class="controls">
+            <input type="text" name="firstname" class="input-xlarge"/>
+          </div>
+        </div>
 
-<p>hello </p>
-<footer> &copy; Holly Liu - 2016</footer>
+        <div class="control-group">
+          <label class="control-label">Last name: </label>
+          <div class="controls">
+            <input type="text" name="lastname" class="input-xlarge"/>
+          </div>
+        </div>
+
+
+        <div class="control-group">
+          <label class="control-label">Email: </label>
+          <div class="controls">
+            <input type="text" name="email" class="input-xlarge"/>
+          </div>
+        </div>
+
+        <div class="control-group">
+          <label class="control-label">Comment: </label>
+          <div class="controls">
+            <input type="text" name="comment" class="input-xlarge"/>
+          </div>
+        </div>
+
+        <div class="control-group">
+          <div class="controls">
+            <input type="submit" value="Add" class="btn btn-primary"/>
+          </div>
+        </div>
+      </form>
+
+
+
+
+  <footer> &copy; Holly Liu - 2016</footer>
 </body>
 </html>
